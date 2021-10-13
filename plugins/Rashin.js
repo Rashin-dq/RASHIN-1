897cc7e58d731dcc3bdb01bc1d08055f1f72b250
Rashin-dq <92039707+Rashin-dq@users.noreply.github.com>
2021-10-13 14:35:20 +0530

    Update Rashin.js

--- plugins/Rashin.js
+++ plugins/Rashin.js
@@ -2,8 +2,7 @@
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-Amalser - Rashin,farhan
+ - Rashin,farhan
 */
 
 const Asena = require('../events');
